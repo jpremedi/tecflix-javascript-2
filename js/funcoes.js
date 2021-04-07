f71f52308b160889efc12216556b80b953b5e5b4
João Pedro Remedi Mota <jpremedi@hotmail.com>
2021-04-06 21:41:10 -0300

    criação da função AdicionarFilme

---
+++ js/funcoes.js
@@ -0,0 +1,9 @@
+function AdicionarFilme() {
+    let campoFilme = document.querySelector("#filme");
+    let imagemFilme = campoFilme.value;
+    ListarFilmesNaTela();
+}
+
+function ListarFilmesNaTela() {
+    console.log("olá");
+}
